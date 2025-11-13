386a900ca2df93a461800713a3baf71f120274f2
Avnishkr01 <avnish011kumar.com>
2025-11-13 15:39:04 +0530

    first commit

--- Client/src/util/api.js
+++ Client/src/util/api.js
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://localhost:5000/api";
+// export const BASE_URL = "http://localhost:5000/api";
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const AUTH_PATHS = {
     AUTH:{
